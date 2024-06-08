9c2d51196289128b78fce1478a5a9f4221351f2c
carlos676767 <carlitobr730@gmail.com>
2024-06-08 12:47:48 -0700

    ok

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,34 @@
-const express = require('express')
-const axios = require("axios")
-const api = express()
-api.get("/", (res, msg) +> {
-    
-})
+const express = require("express");
+const axios = require("axios");
+const api = express();
+const nodemailer = require('nodemailer');
+const { MongoClient } = require("mongodb");
 
+// const cliente =  new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+api.get("/", (res, msg) => {
+  try {
+    msg.status(200).send({ sucesso: "http request sucess" });
+  } catch (error) {
+    msg.status(404).send({ sucesso: "http request error" });
+  }
+});
 
+const httpRequest = async (callback) => {
+  try {
+    const data = await axios.get("http://localhost:8080/");
+    const response = await data.data;
+    callback(response);
+  } catch (error) {
+    console.error("Error http request get from axios.");
+  }
+};
 
+httpRequest((data) => {
+  console.log(data);
+});
 
-const port = 8080
+const port = 8080;
 api.listen(8080, () => {
-    console.log("servidor rodando na porta 8080")
-})
+  console.log("servidor rodando na porta 8080");
+});
