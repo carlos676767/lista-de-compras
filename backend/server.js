f99e376ae5e30e7d0cd49f7d08832b75d65ca04d
carlos676767 <carlitobr730@gmail.com>
2024-06-08 12:23:59 -0700

    ok

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const axios = require("axios")
+const api = express()
+api.get("")
+
+
+
+
+
+const port = 8080
+api.listen(8080, () => {
+    console.log("servidor rodando na porta 8080")
+})
